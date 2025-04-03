70ef65c3f8ac3937e3f6e184d5605e60f7aa7af2
Liyu Wu <liyu.wu@edu.fh-joanneum.at>
2025-04-03 12:16:08 +0200

    initialize a Logging Object with Level Info und Error

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,6 +1,10 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main{
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         Calculator c1 = new Calculator();
         System.out.println(c1.add(1.5,3.4));
@@ -9,5 +13,10 @@ public class Main{
         System.out.println(c1.divide(30,5));
 
         System.out.println("Wu Liyu");
+
+        logger.info("This is an info log");
+        logger.error("This is an error log");
+
+
     }
-}
+}    
